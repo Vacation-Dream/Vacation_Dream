265ed05e80d33e9e8910cc6acd3984544314916e
다빈 <prospecs1039@daum.net>
2023-10-05 15:21:06 +0900

    start

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("스토리 설명...");
+
+
+        System.out.println("===================");
+        System.out.println("# 1일차 아침...\uD83D\uDE06");
+        System.out.println("영희 ");
+        System.out.println("1. 영희집스토킹");
+        System.out.println("2. 낮잠때리기");
+        System.out.println("3. 혼자 산책가기");
+        System.out.println("# 입력: ");
+
+    }
+}
