3da9b61d9714a3c1c5ad69bd72f8ff4de059ae27
다빈 <prospecs1039@daum.net>
2023-10-07 14:19:11 +0900

    # 수정

--- src/Play.java
+++ src/Play.java
@@ -21,11 +21,11 @@ public class Play {
 //        timeString("이곳은 평화로운 시골 동네로, 주인공은 조용한 여름 휴가를 즐기려고 기대합니다.");
 //        timeString("하지만 할머니댁 옆집에 사는 서아라는 손녀를 만나게 됩니다.");
 //        timeString("서아는 동네에서 유명한 미녀로,");
-//        timeString( "그녀의 아름다움과 매력에 주인공은 반한 것 같습니다.");
+//        timeString("그녀의 아름다움과 매력에 주인공은 반한 것 같습니다.");
 //        pressEnter();
 
         System.out.println("==================");
-        timeString("# 방학목표: 축제에서 서아에게 고백하기");
+        timeString("# 목표: 7일차 저녁, 축제에서 서아에게 고백하기");
         System.out.print("# 당신의 이름을 정해주세요: ");
         Variables.name = sc.nextLine();
     }
@@ -34,7 +34,7 @@ public class Play {
     private static void dayStart() {
         System.out.println();
         if (Variables.dayplus.equals("아침")) System.out.print("☀\uFE0F ");
-        else if (Variables.dayplus.equals("오후")) System.out.print("⛅");
+        else if (Variables.dayplus.equals("오후")) System.out.print("⛅ ");
         else System.out.print("\uD83C\uDF19 ");
         System.out.printf("%d일차 %s입니다. \n", Variables.today, Variables.dayplus);
         Util.loveCheck();
